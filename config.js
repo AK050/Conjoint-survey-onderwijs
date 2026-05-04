bb6d4204029d650bc15d3c37de959a234f7617ae
AK050 <a.koller@windesheim.nl>
2026-05-04 22:46:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
+
 const CBC_CONFIG = {
-  supabaseUrl:  "https://jouw-project-id.supabase.co",
-  supabaseKey:  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
-  adminCode:    "kies-een-eigen-wachtwoord"
+  supabaseUrl: "https://nobcpdagesfbasanjulr.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vYmNwZGFnZXNmYmFzYW5qdWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4OTk5NjUsImV4cCI6MjA5MzQ3NTk2NX0.0KInS3iECztfErWINW1s8XGshZaufHy_jMwCC2RKMbI",
+  adminCode: "cbc2025"
 };
