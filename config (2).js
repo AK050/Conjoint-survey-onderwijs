e0e005444ceeab790a5113c1c9a59525286c2d54
AK050 <a.koller@windesheim.nl>
2026-05-04 22:36:50 +0200

    Add files via upload

---
+++ config (2).js
@@ -0,0 +1,6 @@
+// config.js — Voeg dit bestand toe aan je GitHub repository
+const CBC_CONFIG = {
+  supabaseUrl: "https://nobcpdagesfbasanjulr.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vYmNwZGFnZXNmYmFzYW5qdWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4OTk5NjUsImV4cCI6MjA5MzQ3NTk2NX0.0KInS3iECztfErWINW1s8XGshZaufHy_jMwCC2RKMbI",
+  adminCode: "cbc2025"
+};
