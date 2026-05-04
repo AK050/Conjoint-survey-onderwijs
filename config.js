7f336ab18f9ce62228bb3ae887e785ed691fcec1
AK050 <a.koller@windesheim.nl>
2026-05-04 22:39:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
 // Stap 4: Kies een eigen beheercode (wachtwoord voor de admin-pagina)
 
 const CBC_CONFIG = {
-  supabaseUrl:  "PLAK_HIER_JE_PROJECT_URL",       // bijv. https://abcdefgh.supabase.co
-  supabaseKey:  "PLAK_HIER_JE_ANON_PUBLIC_KEY",   // begint met eyJhbG...
+  supabaseUrl:  "https://nobcpdagesfbasanjulr.supabase.co",      // bijv. https://abcdefgh.supabase.co
+  supabaseKey:  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vYmNwZGFnZXNmYmFzYW5qdWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4OTk5NjUsImV4cCI6MjA5MzQ3NTk2NX0.0KInS3iECztfErWINW1s8XGshZaufHy_jMwCC2RKMbI",   // begint met eyJhbG...
   adminCode:    "cbc2025"                          // verander dit naar een eigen wachtwoord
 };
